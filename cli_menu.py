9844eef7c612f1aaee37f73d3a43d70c5bca755c
just cookie file <lorgarhorusov@gmail.com>
2024-06-08 16:33:44 +0600

    новый способ хранения данных, cli меню, обновлен readme файл

--- cli_menu.py
+++ cli_menu.py
@@ -1,11 +1,13 @@
+import sys
+
 import keyring
 import os
 import logging
 import secrets
 
+from user_request import user_request
 from keyring import errors
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
 from InquirerPy.validator import EmptyInputValidator
 
 logging.basicConfig(level=logging.INFO)
@@ -28,97 +30,146 @@ def python_execute(command: str) -> None:
 
 def setting() -> None:
     clear()
-    try:
-        menu = inquirer.select(
-            message="Выберите пункт",
-            choices=["Настройки токена",
-                     "Настройки соли",
-                     "Настройки API (coming soon)",
-                     "Создать БД",
-                     "Назад"],
-        ).execute()
+    menu = inquirer.select(
+        message="Выберите пункт",
+        choices=["Настройки токена",
+                 "Настройки соли",
+                 "Настройки API",
+                 "Настройки языка (coming soon)",
+                 "Создать БД",
+                 "Назад"],
+    ).execute()
 
-        if menu == "Настройки токена":
-            telegram_token_setting()
-        elif menu == "Настройки соли":
-            salt_setting()
-        elif menu == "Создать БД":
-            create_bd()
-        elif menu == "Назад":
-            main()
-    except KeyboardInterrupt:
-        clear()
-        logger.error("экстренное закрытие программы")
+    if menu == "Настройки токена":
+        telegram_token_setting()
+    elif menu == "Настройки соли":
+        salt_setting()
+    elif menu == "Создать БД":
+        create_bd()
+    elif menu == "Настройки API":
+        api_setting()
+    elif menu == "Назад":
+        main()
 
 
 def salt_setting() -> None:
-    sub_menu = inquirer.select(
-        message="Выберите пункт",
-        choices=["Сгенерировать соль",
-                 "Назад"],
-    ).execute()
-    if sub_menu == "Сгенерировать соль":
-        confirm = inquirer.confirm(
-            message="Вы уверены, что хотите сгенерировать новую соль?\n"
-                    "Это приведет к тому, что база данных станет недоступной.",
-            default=False
+    while True:
+        sub_menu = inquirer.select(
+            message="Выберите пункт",
+            choices=["Сгенерировать соль",
+                     "Назад"],
         ).execute()
-        if confirm:
-            salt = secrets.token_hex(32)
-            keyring.set_password('liftes_bot', 'salt', salt)
-            logger.info('Соль сгенерирована и сохранена')
-            salt_setting()
-        else:
-            salt_setting()
-    elif sub_menu == "Назад":
-        setting()
+        if sub_menu == "Сгенерировать соль":
+            confirm = inquirer.confirm(
+                message="Вы уверены, что хотите сгенерировать новую соль?\n"
+                        "Это приведет к тому, что база данных станет недоступной.",
+                default=False
+            ).execute()
+            if confirm:
+                salt = secrets.token_hex(32)
+                keyring.set_password('liftes_bot', 'salt', salt)
+                logger.info('Соль сгенерирована и сохранена')
+                break
+            else:
+                clear()
+        elif sub_menu == "Назад":
+            setting()
 
 
 def telegram_token_setting() -> None:
-    sub_menu = inquirer.select(
-        message="Выберите пункт",
-        choices=["Новый токен",
-                 "Удалить токен",
-                 "Назад"],
-    ).execute()
-    if sub_menu == "Новый токен":
-        clear()
-        old_token = keyring.get_password('liftes_bot', 'telegram_token')
-        if old_token is None:
-            old_token = ''
-        new_token = inquirer.secret(
-            message="пожалуйста, введите токен",
-            default=old_token,
-            validate=EmptyInputValidator()
+    while True:
+        sub_menu = inquirer.select(
+            message="Выберите пункт",
+            choices=["Новый токен",
+                     "Удалить токен",
+                     "Назад"],
         ).execute()
+        if sub_menu == "Новый токен":
+            clear()
+            old_token = keyring.get_password('liftes_bot', 'telegram_token')
+            if old_token is None:
+                old_token = ''
+            new_token = inquirer.secret(
+                message="пожалуйста, введите токен",
+                default=old_token,
+                validate=EmptyInputValidator()
+            ).execute()
 
-        keyring.set_password('liftes_bot', 'telegram_token', new_token)
-        logger.info('Токен сохранен')
-        telegram_token_setting()
+            keyring.set_password('liftes_bot', 'telegram_token', new_token)
+            logger.info('Токен сохранен')
 
-    elif sub_menu == "Удалить токен":
-        clear()
-        confirm = inquirer.confirm(
-            message='Вы уверены?',
-            default=False
-        ).execute()
+        elif sub_menu == "Удалить токен":
+            clear()
+            confirm = inquirer.confirm(
+                message='Вы уверены?',
+                default=False
+            ).execute()
 
-        if confirm:
-            try:
-                keyring.delete_password('liftes_bot', 'telegram_token')
-                logger.info('Токен удален')
-            except keyring.errors.PasswordDeleteError as e:
-                logger.error(f'Ошибка при удалении токена {e}')
-            finally:
-                telegram_token_setting()
-        else:
-            telegram_token_setting()
-    elif sub_menu == "Назад":
-        setting()
+            if confirm:
+                try:
+                    keyring.delete_password('liftes_bot', 'telegram_token')
+                    logger.info('Токен удален')
+
+                except keyring.errors.PasswordDeleteError as e:
+                    logger.error(f'Ошибка при удалении токена {e}')
+
+        elif sub_menu == "Назад":
+            setting()
 
 
 def create_bd() -> None:
     python_execute('main.py --create_table')
+
+
+def api_setting() -> None:
+    while True:
+        sub_menu = inquirer.select(
+            message="Выберите пункт",
+            choices=[
+                "Новый API",
+                "Удалить API",
+                "Тестовый запрос API",
+                "Назад"
+            ]
+        ).execute()
+
+        if sub_menu == "Новый API":
+            clear()
+            old_api = keyring.get_password('liftes_bot', 'liftes_api')
+
+            if old_api is None:
+                old_api = ''
+            new_api = inquirer.text(
+                message='Введите новый API',
+                default=old_api,
+                validate=EmptyInputValidator()
+            ).execute()
+            keyring.set_password('liftes_bot', 'liftes_api', new_api)
+            logger.info('API Установлен')
+
+        elif sub_menu == "Удалить API":
+            clear()
+            confirm = inquirer.confirm(
+                message='Вы уверены?',
+                default=False
+            ).execute()
+
+            if confirm:
+                try:
+                    keyring.delete_password('liftes_bot', 'liftes_api')
+                    logger.info('Пароль удален')
+                except keyring.errors.PasswordDeleteError as e:
+                    logger.error(e)
+
+        elif sub_menu == "Тестовый запрос API":
+            phone = inquirer.text(
+                message='Введите номер телефона'
+            ).execute()
+            response = user_request(phone)
+            logger.info(response)
+
+        elif sub_menu == "Назад":
+            setting()
 
 
 def main() -> None:
@@ -129,15 +180,16 @@ def main() -> None:
             choices=[
                 "Запуск бота",
                 "Настройки",
-                Choice(value=None, name='Выход'),
+                "Выход",
             ],
-            default=None
         ).execute()
 
         if menu == "Запуск бота":
             python_execute('main.py --start_bot')
         elif menu == "Настройки":
             setting()
+        elif "Выход":
+            sys.exit()
 
     except KeyboardInterrupt:
         clear()
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 
 from nacl.hash import blake2b
 from nacl.encoding import HexEncoder
+from user_request import user_request
 
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, \
     ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
@@ -39,18 +40,22 @@ def create_table() -> None:
 
 
 def generate_salt() -> None:
+    attempts = 5
     while True:
-        confirm = input('Are you sure you want to generate a new salt?'
-                        ' This will cause the database to become inaccessible y/n: ')
-        if confirm.lower() == 'y':
-            salt = secrets.token_hex(32)
-            keyring.set_password('liftes_bot', 'salt', salt)
-            logger.info('Соль сгенерирована и сохранена')
-            break
-        elif confirm.lower() == 'n':
-            sys.exit()
-        else:
-            print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+        for i in range(5):
+            confirm = input('Are you sure you want to generate a new salt?'
+                            ' This will cause the database to become inaccessible y/n: ')
+            if confirm.lower() == 'y':
+                salt = secrets.token_hex(32)
+                keyring.set_password('liftes_bot', 'salt', salt)
+                logger.info('Соль сгенерирована и сохранена')
+                break
+            elif confirm.lower() == 'n':
+                sys.exit()
+            else:
+                attempts -= 1
+                print(f"Invalid input. Please enter 'y' for yes or 'n' for no.\nAttempts {attempts}")
+        break
 
 
 def add_new_user(telegram_id: str, user_phone: str) -> None:
@@ -146,9 +151,11 @@ def start_bot() -> None:
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--create_table', action='store_true', help='Create table')
-    parser.add_argument('--salt', action='store_true', help='generate salt')
-    parser.add_argument('--start_bot', action='store_true', help='bot launch')
+    parser.add_argument('-Ct', '--create_table', action='store_true', help='Create table')
+    parser.add_argument('-S', '--salt', action='store_true', help='Generate salt')
+    parser.add_argument('-Sb', '--start_bot', action='store_true', help='Launch bot')
+    parser.add_argument('-Sa', '--set_api', help='Set API URL')
+    parser.add_argument('-T', '--test', help='Phone number for test API')
     args = parser.parse_args()
 
     if args.create_table:
@@ -157,6 +164,17 @@ def main() -> None:
         generate_salt()
     elif args.start_bot:
         start_bot()
+    elif args.test:
+        if args.test is None:
+            print('Please use --test <phone number>')
+            sys.exit()
+        response = user_request(args.test)
+        print(response)
+    elif args.set_api:
+        if args.set_api is None:
+            print('Please use --set_api <api url>')
+            sys.exit()
+        keyring.set_password('liftes_bot', 'liftes_api', args.set_api)
     else:
         print('No arguments provided')
 
