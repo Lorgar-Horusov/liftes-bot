f36d60ba395b86e22de3917bb4c56e514d764569
just cookie file <lorgarhorusov@gmail.com>
2024-06-12 21:27:19 +0600

    переписал половину кода :C

--- cli_menu.py
+++ cli_menu.py
@@ -3,12 +3,13 @@ import sys
 import keyring
 import os
 import logging
-import secrets
 
-from user_request import user_request
+from user_request import ClientInfo
 from keyring import errors
 from InquirerPy import inquirer
 from InquirerPy.validator import EmptyInputValidator
+from nacl.secret import SecretBox
+from nacl.utils import random
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('OwO logger')
@@ -21,6 +22,13 @@ def clear() -> None:
         os.system('clear')
 
 
+def pause() -> None:
+    if os.name == 'nt':
+        os.system('pause')
+    elif os.name == 'posix':
+        os.system('read -n1 -r -p "Нажмите любую клавишу, чтобы продолжить..." key')
+
+
 def python_execute(command: str) -> None:
     if os.name == 'nt':
         os.system(f'python {command}')
@@ -29,51 +37,72 @@ def python_execute(command: str) -> None:
 
 
 def setting() -> None:
-    clear()
-    menu = inquirer.select(
-        message="Выберите пункт",
-        choices=["Настройки токена",
-                 "Настройки соли",
-                 "Настройки API",
-                 "Настройки языка (coming soon)",
-                 "Создать БД",
-                 "Назад"],
-    ).execute()
+    while True:
+        clear()
+        menu = inquirer.select(
+            message="Выберите пункт",
+            choices=["Настройки токена",
+                     "Настройки ключа",
+                     "Настройки API",
+                     "Настройки языка (coming soon)",
+                     "Создать БД",
+                     "Назад"],
+        ).execute()
+        match menu:
+            case "Настройки токена":
+                telegram_token_setting()
+            case "Настройки ключа":
+                key_setting()
+            case "Создать БД":
+                create_bd()
+            case "Настройки API":
+                api_setting()
+            case _:
+                main()
 
-    if menu == "Настройки токена":
-        telegram_token_setting()
-    elif menu == "Настройки соли":
-        salt_setting()
-    elif menu == "Создать БД":
-        create_bd()
-    elif menu == "Настройки API":
-        api_setting()
-    elif menu == "Назад":
-        main()
+
+def generate_new_key() -> None:
+    key = random(SecretBox.KEY_SIZE)
+    keyring.set_password('liftes_bot', 'key', key.hex())
+    logger.info('ключ сгенерирован и сохранен\nВаш ключ')
+    logger.info(keyring.get_password('liftes_bot', 'key'))
 
 
-def salt_setting() -> None:
+def key_setting() -> None:
     while True:
         sub_menu = inquirer.select(
             message="Выберите пункт",
-            choices=["Сгенерировать соль",
+            choices=["Сгенерировать ключ",
+                     "Ввести ключ",
                      "Назад"],
         ).execute()
-        if sub_menu == "Сгенерировать соль":
-            confirm = inquirer.confirm(
-                message="Вы уверены, что хотите сгенерировать новую соль?\n"
-                        "Это приведет к тому, что база данных станет недоступной.",
-                default=False
-            ).execute()
-            if confirm:
-                salt = secrets.token_hex(32)
-                keyring.set_password('liftes_bot', 'salt', salt)
-                logger.info('Соль сгенерирована и сохранена')
-                break
-            else:
+        match sub_menu:
+            case "Сгенерировать ключ":
+                confirm = inquirer.confirm(
+                    message="Вы уверены, что хотите сгенерировать новый ключ?\n"
+                            "Это приведет к тому, что база данных станет недоступной.",
+                    default=False
+                ).execute()
+                if confirm:
+                    generate_new_key()
+                    break
+                else:
+                    clear()
+
+            case "Ввести ключ":
                 clear()
-        elif sub_menu == "Назад":
-            setting()
+                old_key = keyring.get_password('liftes_bot', 'key')
+                if old_key is None:
+                    old_key = ''
+                new_key = inquirer.secret(
+                    message='Введите ключ',
+                    default=old_key,
+                    validate=EmptyInputValidator()
+                ).execute()
+                keyring.set_password('liftes_bot', 'key', new_key)
+
+            case _:
+                setting()
 
 
 def telegram_token_setting() -> None:
@@ -84,37 +113,38 @@ def telegram_token_setting() -> None:
                      "Удалить токен",
                      "Назад"],
         ).execute()
-        if sub_menu == "Новый токен":
-            clear()
-            old_token = keyring.get_password('liftes_bot', 'telegram_token')
-            if old_token is None:
-                old_token = ''
-            new_token = inquirer.secret(
-                message="пожалуйста, введите токен",
-                default=old_token,
-                validate=EmptyInputValidator()
-            ).execute()
+        match sub_menu:
+            case "Новый токен":
+                clear()
+                old_token = keyring.get_password('liftes_bot', 'telegram_token')
+                if old_token is None:
+                    old_token = ''
+                new_token = inquirer.secret(
+                    message="пожалуйста, введите токен",
+                    default=old_token,
+                    validate=EmptyInputValidator()
+                ).execute()
 
-            keyring.set_password('liftes_bot', 'telegram_token', new_token)
-            logger.info('Токен сохранен')
+                keyring.set_password('liftes_bot', 'telegram_token', new_token)
+                logger.info('Токен сохранен')
 
-        elif sub_menu == "Удалить токен":
-            clear()
-            confirm = inquirer.confirm(
-                message='Вы уверены?',
-                default=False
-            ).execute()
+            case "Удалить токен":
+                clear()
+                confirm = inquirer.confirm(
+                    message='Вы уверены?',
+                    default=False
+                ).execute()
 
-            if confirm:
-                try:
-                    keyring.delete_password('liftes_bot', 'telegram_token')
-                    logger.info('Токен удален')
+                if confirm:
+                    try:
+                        keyring.delete_password('liftes_bot', 'telegram_token')
+                        logger.info('Токен удален')
 
-                except keyring.errors.PasswordDeleteError as e:
-                    logger.error(f'Ошибка при удалении токена {e}')
+                    except keyring.errors.PasswordDeleteError as e:
+                        logger.error(f'Ошибка при удалении токена {e}')
 
-        elif sub_menu == "Назад":
-            setting()
+            case _:
+                setting()
 
 
 def create_bd() -> None:
@@ -133,43 +163,46 @@ def api_setting() -> None:
             ]
         ).execute()
 
-        if sub_menu == "Новый API":
-            clear()
-            old_api = keyring.get_password('liftes_bot', 'liftes_api')
+        match sub_menu:
+            case "Новый API":
+                clear()
+                old_api = keyring.get_password('liftes_bot', 'liftes_api')
 
-            if old_api is None:
-                old_api = ''
-            new_api = inquirer.text(
-                message='Введите новый API',
-                default=old_api,
-                validate=EmptyInputValidator()
-            ).execute()
-            keyring.set_password('liftes_bot', 'liftes_api', new_api)
-            logger.info('API Установлен')
+                if old_api is None:
+                    old_api = ''
+                new_api = inquirer.text(
+                    message='Введите новый API',
+                    default=old_api,
+                    validate=EmptyInputValidator()
+                ).execute()
+                keyring.set_password('liftes_bot', 'liftes_api', new_api)
+                logger.info('API Установлен')
 
-        elif sub_menu == "Удалить API":
-            clear()
-            confirm = inquirer.confirm(
-                message='Вы уверены?',
-                default=False
-            ).execute()
+            case "Удалить API":
+                clear()
+                confirm = inquirer.confirm(
+                    message='Вы уверены?',
+                    default=False
+                ).execute()
 
-            if confirm:
-                try:
-                    keyring.delete_password('liftes_bot', 'liftes_api')
-                    logger.info('Пароль удален')
-                except keyring.errors.PasswordDeleteError as e:
-                    logger.error(e)
+                if confirm:
+                    try:
+                        keyring.delete_password('liftes_bot', 'liftes_api')
+                        logger.info('Пароль удален')
+                    except keyring.errors.PasswordDeleteError as e:
+                        logger.error(e)
 
-        elif sub_menu == "Тестовый запрос API":
-            phone = inquirer.text(
-                message='Введите номер телефона'
-            ).execute()
-            response = user_request(phone)
-            logger.info(response)
+            case "Тестовый запрос API":
+                phone = inquirer.text(
+                    message='Введите номер телефона'
+                ).execute()
+                clear()
+                client_info = ClientInfo(phone)
+                logger.info(f'Адрес: {client_info.address}\nИмя: {client_info.name}')
+                pause()
 
-        elif sub_menu == "Назад":
-            setting()
+            case _:
+                setting()
 
 
 def main() -> None:
@@ -184,12 +217,13 @@ def main() -> None:
             ],
         ).execute()
 
-        if menu == "Запуск бота":
-            python_execute('main.py --start_bot')
-        elif menu == "Настройки":
-            setting()
-        elif "Выход":
-            sys.exit()
+        match menu:
+            case "Запуск бота":
+                python_execute('main.py --start_bot')
+            case "Настройки":
+                setting()
+            case _:
+                sys.exit()
 
     except KeyboardInterrupt:
         clear()
